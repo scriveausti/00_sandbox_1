8cc42b5d719e339016ed9a4f94fcb56cd5b0797b
scriveausti <scrive.austi@awatapu.school.nz>
2022-02-28 09:29:54 +1300

    guess number

---
+++ 07_guess_number.py
@@ -0,0 +1,26 @@
+import random
+
+number = random.randint(1,10)
+players_guess = 0
+lives = 3
+guess = 0
+
+print(number)
+print("")
+
+while players_guess != number and guess != lives:
+    try:
+        players_guess = int(input("guess the number between 1 and 10 "))
+    except:
+        print("<error> pleas enter a number")
+        print("")
+    if players_guess == number:
+        print("nice you got it right")
+    else:
+        print("incorrect")
+        guess = guess + 1
+        print("you have {} lives left".format(lives - guess))
+        print("")
+
+if guess == lives:
+    print("you ran out of lives better luck next time")
